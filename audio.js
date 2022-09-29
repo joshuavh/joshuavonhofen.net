acafad67a84e9346de06a3eaa26e9b871495e32a
joshuavh <joshua.vonhofen@gmail.com>
2022-09-29 18:47:49 +0200

    Added to netlify

--- audio.js
+++ audio.js
@@ -1,56 +1,174 @@
-const playerKick = new Tone.Player("Roland TR-909/BTAAADA.WAV").toDestination();
-const playerClap = new Tone.Player("Roland TR-909/HANDCLP1.WAV").toDestination();
-const playerHiHat = new Tone.Player("Roland TR-909/HHOD0.WAV").toDestination();
+const makeSynths = (count) => {
+  // declare array to store synths
+  const synths = [];
+	// I'll be using a one octive F minor pentatonic scale
+  // so I'll need 6 synths
+  for (let i = 0; i < count; i++) {
+    let synth = new Tone.Sampler({
+      urls: {
+        "F4": "Roland TR-909/RIM127.WAV",
+        "Eb4": "Roland TR-909/HT0D0.WAV",
+        "C4": "Roland TR-909/RIDEDA.WAV", 
+        "Bb3": "Roland TR-909/HHCD2.WAV",
+        "Ab3": "Roland TR-909/HANDCLP1.WAV", 
+        "F3": "Roland TR-909/BTAAADA.WAV"
+      },
+      release: 1,
+    }).toDestination();
+      
+   
+    synths.push(synth);
+  }
+
+  return synths;
+};
+
+const makeGrid = (notes) => {
+  // our "notation" will consist of an array with 6 sub arrays
+  // each sub array corresponds to one row in our sequencer grid
+
+  // parent array to hold each row subarray
+  const rows = [];
+
+  for (const note of notes) {
+    // declare the subarray
+    const row = [];
+    // each subarray contains multiple objects that have an assigned note
+    // and a boolean to flag whether they are "activated"
+    // each element in the subarray corresponds to one eigth note
+    for (let i = 0; i < 8; i++) {
+      row.push({
+        note: note,
+        isActive: false
+      });
+    }
+    rows.push(row);
+  }
+
+  // we now have 6 rows each containing 16 eighth notes
+  return rows;
+};
+
+const synths = makeSynths(6);
+
+// declaring the notes for each row
+const notes = ["F4", "Eb4", "C4", "Bb3", "Ab3", "F3"];
+let grid = makeGrid(notes);
+let beat = 0;
 let playing = false;
 let started = false;
 
-Tone.Transport.scheduleRepeat((time) => {
-    // use the callback time to schedule events
-    playerKick.start(time).stop(time + 0.5);
-}, "4n");
+const configLoop = () => {
 
-Tone.Transport.scheduleRepeat((time) => {
-    // use the callback time to schedule events
-    playerClap.start(time).stop(time + 0.5);
-}, "2n");
-
-Tone.Transport.scheduleRepeat((time) => {
-    // use the callback time to schedule events
-    playerHiHat.start(time).stop(time + 0.5);
-}, "8n");
-
-
-
-
-const configPlayButton = () => {
-    // grab the button from the DOM
-    const button = document.getElementById("play-button");
-    
-    button.addEventListener("click", (e) => {
-      if (!started) {
-        // Only exectued the first time the button is clicked
-        // initializing Tone, setting the volume, and setting up the loop
-        
-        Tone.start();
-        Tone.getDestination().volume.rampTo(-10, 0.001)
-        started = true;
-      }
-  
-      // toggle Tone.Trasport and the flag variable.
-      if (playing) {
-        e.target.innerText = "Play";
-        Tone.Transport.stop();
-        playing = false;
-      } else {
-        e.target.innerText = "Stop";
-        Tone.Transport.start();
-        playing = true;
+  const repeat = (time) => {
+    grid.forEach((row, index) => {
+      let synth = synths[index];
+      let note = row[beat];
+      if (note.isActive) {
+        synth.triggerAttackRelease(note.note, "8n", time);
       }
     });
+
+    beat = (beat + 1) % 8;
   };
 
-  /* configPlayButton();
+  Tone.Transport.bpm.value = 120;
+  Tone.Transport.scheduleRepeat(repeat, "8n");
+};
+
+let i = 0;
+
+const makeSequencer = () => {
+  const sequencer = document.getElementById("sequencer");
+  grid.forEach((row, rowIndex) => {
+    const seqRow = document.createElement("div");
+    seqRow.id = `rowIndex`;
+    seqRow.className = "sequencer-row";
+
+    row.forEach((note, noteIndex) => {
+      const button = document.createElement("button");
+      console.log(note);
+      if (note.note == "F4"){
+        button.innerHTML = "Rim"
+      }
+      if (note.note == "Eb4"){
+        button.innerHTML = "Tom"
+      }
+      if (note.note == "C4"){
+        button.innerHTML = "Ride"
+      }
+      if (note.note == "Bb3"){
+        button.innerHTML = "Hat"
+      }
+      if (note.note == "Ab3"){
+        button.innerHTML = "Clap"
+      }
+      if (note.note == "F3"){
+        button.innerHTML = "Kick"
+      }
+
+      button.className = "note"
+      button.addEventListener("click", function(e) {
+        handleNoteClick(rowIndex, noteIndex, e);
+      });
+
+      seqRow.appendChild(button);
+      i++;
+    });
+
+    sequencer.appendChild(seqRow);
+  });
+};
+
+const handleNoteClick = (clickedRowIndex, clickedNoteIndex, e) => {
+  grid.forEach((row, rowIndex) => {
+    row.forEach((note, noteIndex) => {
+      if (clickedRowIndex === rowIndex && clickedNoteIndex === noteIndex) {
+        note.isActive = !note.isActive;
+        if (note.isActive){
+          e.target.classList.add("note-is-active");
+        }
+        else{
+          e.target.classList.remove("note-is-active");
+        }
+        
+        // e.target.className = classNames(
+        //   "note", 
+        //   { "note-is-active": !!note.isActive }, 
+        //   { "note-not-active": !note.isActive }
+        // );
+      }
+    });
+  });
+};
+
+const configPlayButton = () => {
+  const button = document.getElementById("play-button");
+  button.addEventListener("click", (e) => {
+    if (!started) {
+      Tone.start();
+      Tone.getDestination().volume.rampTo(-10, 0.001)
+      configLoop();
+      started = true;
+    }
+
+    if (playing) {
+      e.target.innerText = "► Play";
+      e.target.style.backgroundColor = "black";
+      Tone.Transport.stop();
+      playing = false;
+    } else {
+      e.target.innerText = "■ Stop";
+      e.target.style.backgroundColor = "blue";
+      Tone.Transport.start();
+      playing = true;
+    }
+  });
+};
+
+/* configPlayButton();
 makeSequencer(); */
 window.addEventListener("DOMContentLoaded", () => {
-    configPlayButton();
-  });
+  configPlayButton();
+	makeSequencer();
+});
